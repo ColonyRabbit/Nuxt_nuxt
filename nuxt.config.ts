436e532514e71d8af1dc40a951982c792be50bc7
khanakorn <khanakorn01@gmail.com>
2025-03-22 23:47:09 +0700

    add tailwind and early learning about nuxt3

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primeuix/themes/aura";
+
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template lang="">
+  <div>
+    <h1>Landing Pages</h1>
+  </div>
+</template>
+
+<script setup></script>
+<style lang=""></style>
